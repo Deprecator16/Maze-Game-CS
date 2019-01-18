95e6558fb74d98c19b79044ac1aba169d2632093
Bassil Virk <bassillooney@outlook.com>
2019-01-17 22:14:58 -0500

    Created Project
    Created Project and added main()

---
+++ MazeGame/src/mazegame/MazeGame.java
@@ -0,0 +1,17 @@
+/*
+ * MazeGame.java
+ */
+package mazegame;
+
+public class MazeGame
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
