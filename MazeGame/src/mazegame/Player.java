0fd13017b1a1d6ca1df7c7edeb6db8669d5eb17b
Bassil Virk <bassillooney@outlook.com>
2019-01-19 13:27:59 -0500

    Completed Javadoc and methods for Player class
    Created Javadoc for Player class and completed all methods as well.

--- MazeGame/src/mazegame/Player.java
+++ MazeGame/src/mazegame/Player.java
@@ -15,23 +15,43 @@ public class Player
 {
    private Rectangle player;
    
+   /** Offsets player position by x,y amount
+    * 
+    * @param x offset player on the x-axis by x amount
+    * @param y offset player on the y-axis by y amount
+    */
    public void updatePos(int x, int y)
    {
-       
+       player.x += x;
+       player.y += y;
    }
    
+   /** Sets position of player
+    * 
+    * @param x set player coordinates to position x on x-axis
+    * @param y set player coordinates to position y on y-axis
+    */
    public void setPos(int x, int y)
    {
-       
+       player.x = x;
+       player.y = y;
    }
    
+   /** Sets size of player
+    * 
+    * @param size size of player square
+    */
    public void setSize(int size)
    {
-       
+       player.setSize(size, size);
    }
    
+   /**
+    * Constructor
+    */
    public Player()
    {
-       
+       player = new Rectangle();
+       setSize(4);
    }
 }
